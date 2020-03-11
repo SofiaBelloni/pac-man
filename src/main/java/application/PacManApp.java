0ce1b7325147cc2861fc6bc80f384836f51db893
FILIPPO VISSANI <filippo.vissani@studio.unibo.it>
2020-03-11 18:23:11 +0100

    test git

--- src/main/java/application/PacManApp.java
+++ src/main/java/application/PacManApp.java
@@ -11,17 +11,18 @@ import javafx.stage.Stage;
  */
 public final class PacManApp extends Application {
 
-    private static final int SCENE_WIDTH = 500;
-    private static final int SCENE_HEIGHT = 300;
+//    private static final int SCENE_WIDTH = 500;
+//    private static final int SCENE_HEIGHT = 300;
 
     @Override
     public void start(final Stage stage) throws Exception {
-        final Parent root = FXMLLoader.load(ClassLoader.getSystemResource("layouts/main.fxml"));
-        final Scene scene = new Scene(root, SCENE_WIDTH, SCENE_HEIGHT);
-        // Stage configuration
-        stage.setTitle("JavaFX - Complete Example");
-        stage.setScene(scene);
-        stage.show();
+//        final Parent root = FXMLLoader.load(ClassLoader.getSystemResource("layouts/main.fxml"));
+//        final Scene scene = new Scene(root, SCENE_WIDTH, SCENE_HEIGHT);
+//        // Stage configuration
+//        stage.setTitle("JavaFX - Complete Example");
+//        stage.setScene(scene);
+//        stage.show();
+        
     }
 
     /**
