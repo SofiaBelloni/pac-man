42ff3d99019bfb4ebadc3805ab2a2811af96d9e5
Eddie Barzi <eddie.barzi@studio.unibo.it>
2020-03-15 11:24:48 +0100

    Refactor of interface mobile entity

--- src/main/java/model/MobileEntity.java
+++ src/main/java/model/MobileEntity.java
@@ -8,18 +8,4 @@ public interface MobileEntity extends Entity {
      * @param position of the entity
      */
     void setPosition(Pair<Integer, Integer> position);
-//
-//    /**
-//     * Set the mobile entity direction.
-//     * 
-//     * @param direction the new direction of the entity
-//     */
-//    void setDirection(Directions direction);
-//
-//    /**
-//     * Set the mobile entity direction.
-//     * 
-//     * @return the direction of the entity
-//     */
-//    Directions getDirection();
 }
