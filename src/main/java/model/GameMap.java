d02455cb18aab5e8c617ad8575e67486fc17fa1f
FILIPPO VISSANI <filippo.vissani@studio.unibo.it>
2020-03-23 10:17:00 +0100

    Added an interface to manage the game map

---
+++ src/main/java/model/GameMap.java
@@ -0,0 +1,34 @@
+package model;
+
+import java.util.Set;
+
+/**
+ * this interface is used to manage the game map and all its immobile entities.
+ *
+ */
+public interface GameMap {
+    /**
+     * @param position of the pill to remove
+     */
+    void removePill(Pair<Integer, Integer> position);
+    /**
+     * @return the walls positions
+     */
+    Set<Pair<Integer, Integer>> getWallsPositions();
+    /**
+     * @return the pills positions
+     */
+    Set<Pair<Integer, Integer>> getPillsPositions();
+    /**
+     * @return the ghost house position
+     */
+    Set<Pair<Integer, Integer>> getGhostHousePosition();
+    /**
+     * @return the map size on x axis
+     */
+    int getxMapSize();
+    /**
+     * @return the map size on y axis
+     */
+    int getyMapSize();
+}
