891b8f4363356633640f23b15d323dec196a019c
Eddie Barzi <eddie.barzi@studio.unibo.it>
2020-03-24 17:59:37 +0100

    Refactoring of MobileEntity interface

--- src/main/java/model/MobileEntity.java
+++ src/main/java/model/MobileEntity.java
@@ -4,8 +4,8 @@ package model;
  * This interface represents an Entity that can be moved.
  */
 public interface MobileEntity extends Entity {
-    /*
-     * Moves the Entity on the next position
+    /**
+     * Moves the Entity on the next position.
      */
-    int nextPosition();
+    void nextPosition();
 }
