20704399d44bb73070bc67ccb36ceb16c77ae431
Davide Rossi <davide.rossi39@studio.unibo.it>
2020-03-31 20:02:52 +0200

    modified GhostFactoryImpl in a way to associate each Blinky to a Inky

--- src/main/java/model/Inky.java
+++ src/main/java/model/Inky.java
@@ -1,100 +1,53 @@
 package model;
 
-import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 
 public class Inky extends GhostAbstractImpl {
 
-    private Pair<Integer, Integer> appo;
-    private Pair<Integer, Integer> targett;
-    private Pair<Integer, Integer> ris;
-    private int targetX;
-    private int targetY;
     private Pair<Integer, Integer> blinkyPosition;
     Set<Pair<Integer, Integer>> setWall;
+    private boolean isBlinkyDead;
+    private BraveBehaviour blinky;
     
-    public Inky(Map<Pair<Integer, Integer>, Integer> map, Set<Pair<Integer, Integer>> setWall, Blinky blinky) {
-        super(map, setWall);
+    public Inky(Set<Pair<Integer, Integer>> setWall, int xMap, int yMap, Blinky blinky) {
+        super(setWall, xMap, yMap);
         this.setWall = setWall;
-        super.target=new Pair<>(MAX,0);
+        super.target=new Pair<>(xMap,0);
         this.blinkyPosition=blinky.getPosition();
-    }
-    
-    public Pair<Integer,Integer> targetPosition(PacMan pacMan){
-        Pair<Integer, Integer> pacManPosition = pacMan.getPosition();
-        Directions pacManDirection = pacMan.getDirection();
-        if (pacManDirection.equals(Directions.UP)) {
-            appo = new Pair<>(pacManPosition.getX(), pacManPosition.getY() + 2);
-        } else if (pacManDirection.equals(Directions.RIGHT)) {
-            appo = new Pair<>(pacManPosition.getX() + 2, pacManPosition.getY());
-        } else if (pacManDirection.equals(Directions.DOWN)) {
-            appo = new Pair<>(pacManPosition.getX(), pacManPosition.getY() - 2);
-        } else {
-            appo = new Pair<>(pacManPosition.getX() - 2, pacManPosition.getY());
-        }
-        if(blinkyPosition.getX()<=appo.getX()) {
-            targetX=blinkyPosition.getX() + (Math.abs(appo.getX() - blinkyPosition.getX())*2);
-        } else {
-            targetX=blinkyPosition.getX() - (Math.abs(appo.getX() - blinkyPosition.getX())*2);
-        }
-        
-        if(blinkyPosition.getY()<=appo.getY()) {
-            targetY=blinkyPosition.getY() + (Math.abs(appo.getY() - blinkyPosition.getY())*2);
-        } else {
-            targetY=blinkyPosition.getY() - (Math.abs(appo.getY() - blinkyPosition.getY())*2);
-        }
-        ris = new Pair<>(targetX, targetY);
-        
-        if (ris.getX()>MAX) {
-            ris = new Pair<>(MAX, ris.getY());
-        }
-        if (ris.getX()<0) {
-            ris = new Pair<>(0, ris.getY());
-        }
-        if (ris.getY()>MAX) {
-            ris = new Pair<>(ris.getX(), MAX);
-        }
-        if (ris.getY()<0) {
-            ris = new Pair<>(ris.getY(), 0);
-        }   
-        if (setWall.contains(ris) || currentPosition.equals(ris)) {
-            setAdj(ris);
-            if (!setWall.contains(up)) {
-                ris = up;
-            } else if (!setWall.contains(right)) {
-                ris = right;
-            } else if (!setWall.contains(down)) {
-                ris = down;
-            } else {
-                ris = left;
-            }
-        }
-        return ris;
-        
-
+        super.c = new BraveInkyBehaviour(setWall, xMap, yMap);
+        super.initialPosition = new Pair<>(7,6);
     }
 
      public int nextPosition(PacMan pacMan) {
-         targett=targetPosition(pacMan);
-        if(super.isEatable()) {
-            super.runAway();
-        }else {
-            super.min=100000;
-             for (Pair<Integer, Integer> p:map.keySet()) {
-                 map.put(p, 1000);
-             }
-               if (isRelaxed) {
-                   isRelaxed = relax();
-               } else {
-                   findPath(currentPosition, targett, super.dir);
-                   move(targett);
-               }
-        }
-        if (currentPosition.equals(pacMan.getPosition())) {
-              return 1;
-        } else {
-              return 0;
-        }
-
-    }
+         if (isEatable()) {
+             f.runAway(currentPosition, dir);
+                currentPosition = f.getNewPosition();
+                dir = f.getNewDirection();
+            } else {
+                   if (isRelaxed) {
+                       c.relax(currentPosition, target, dir); 
+                        currentPosition = c.getNewPosition();
+                        dir = c.getNewDirection();
+                        if (currentPosition.equals(target)) {
+                            isRelaxed = false;
+                        }
+                   } else {
+                        if (isBlinkyDead) {
+                            blinky.chase(currentPosition, pacMan, dir, Optional.of(blinkyPosition));
+                            currentPosition = blinky.getNewPosition();
+                            dir = blinky.getNewDirection();
+                        } else {
+                            c.chase(currentPosition, pacMan, dir, Optional.of(blinkyPosition));
+                            currentPosition = c.getNewPosition();
+                            dir = c.getNewDirection();
+                        }
+                   }
+            }
+            if (currentPosition.equals(pacMan.getPosition())) {
+                  return 1;
+            } else {
+                  return 0;
+            }
+     }
 }
