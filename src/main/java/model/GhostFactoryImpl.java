7c69e42351ef2d5a65c44307e49853851b94c945
Davide Rossi <davide.rossi39@studio.unibo.it>
2020-03-31 20:50:38 +0200

    modified GhostFactoryImpl in a way to associate each Blinky to a Inky

--- src/main/java/model/GhostFactoryImpl.java
+++ src/main/java/model/GhostFactoryImpl.java
@@ -1,40 +1,45 @@
 package model;
 
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 
 public class GhostFactoryImpl implements GhostFactory {
-    private final Map<Pair<Integer,Integer>, Integer> map;
     private final Set<Pair<Integer,Integer>> setWall;
-    private final Map<Integer, Blinky> blinkies=new HashMap<>();
+    private final int xMap;
+    private final int yMap;
+    private Blinky blinky;
     private Inky inky;
-    private int i = 0;
-    
-    public GhostFactoryImpl(Map<Pair<Integer,Integer>,Integer> map,Set<Pair<Integer,Integer>> setWall) {
-        this.map=map;
-        this.setWall=setWall;
-    }
+    private List<Blinky> listOfBlinky;
 
+    public GhostFactoryImpl(Set<Pair<Integer,Integer>> setWall, Integer xMap, Integer yMap) {
+        this.setWall = setWall;
+        this.xMap = xMap;
+        this.yMap = yMap;
+        this.listOfBlinky = new LinkedList<>();
+    }
+    
     @Override
     public Ghost create(Ghosts ghost) { 
         if (ghost.equals(Ghosts.BLINKY)) {
-            i++;
-            blinkies.put(i, new Blinky(this.map, this.setWall));
-            return blinkies.get(i);
-        }else if (ghost.equals(Ghosts.INKY)) {
-            if (!blinkies.isEmpty()) {
-                inky = new Inky(this.map, this.setWall, blinkies.get(i));
-                blinkies.remove(0);
+            blinky = new Blinky(setWall, xMap, yMap);
+            listOfBlinky.add(blinky);
+            return blinky;
+        } else if (ghost.equals(Ghosts.INKY)) {
+            if (!listOfBlinky.isEmpty()) {
+                inky = new Inky(setWall, xMap, yMap, listOfBlinky.get(0));
+                listOfBlinky.remove(0);
                 return inky;
             } else {
-                throw new IllegalStateException("Must create a Blinky ghost before");
+                throw new IllegalStateException("Must create Blinky before Inky");
             }
-        }else if (ghost.equals(Ghosts.PINKY)) {
-            return new Pinky(this.map, this.setWall);
+        } else if (ghost.equals(Ghosts.PINKY)) {
+            return new Pinky(setWall, xMap, yMap);
         } else {
-            return new Clyde(this.map, this.setWall);
+            return new Clyde(setWall, xMap, yMap);
         }
     }
 }
