108e2918f599c62e781d5c26a099f207dd205cb8
Davide Rossi <davide.rossi39@studio.unibo.it>
2020-03-31 20:02:52 +0200

    modified Inky and Clyde constructors and NextPosiion method

--- src/main/java/model/Clyde.java
+++ src/main/java/model/Clyde.java
@@ -1,37 +1,35 @@
 package model;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 
 public class Clyde extends GhostAbstractImpl {
 
-    public Clyde(final Map<Pair<Integer, Integer>, Integer> map, final Set<Pair<Integer, Integer>> setWall) {
-        super(map, setWall);
-        super.target = new Pair<>(0,0);
-    }
-
-    public Pair<Integer,Integer> targetPosition(PacMan pacMan){
-        if (currentPosition.getX()>pacMan.getPosition().getX() - 4 && currentPosition.getX()<pacMan.getPosition().getX() + 4 
-            && currentPosition.getY()>pacMan.getPosition().getY() - 4 && currentPosition.getY()<pacMan.getPosition().getY() + 4 ) {
-            return super.target;
-        } else {
-            return pacMan.getPosition();
-        }
+    public Clyde(Set<Pair<Integer, Integer>> setWall, Integer xMap, Integer yMap) {
+        super(setWall, xMap, yMap);
+        super.target=new Pair<>(0,0);
+        super.c = new BraveClydeBehaviour(setWall, xMap, yMap, super.target);
+        super.initialPosition = new Pair<>(7,6);
     }
 
      public int nextPosition(PacMan pacMan) {
-            if (super.isEatable()) {
-                super.runAway();
+            if (isEatable()) {
+                f.runAway(currentPosition, dir);
+                currentPosition = f.getNewPosition();
+                dir = f.getNewDirection();
             } else {
-                super.min=100000;
-                 for (Pair<Integer, Integer> p:map.keySet()) {
-                     map.put(p, 1000);
-                 }
                    if (isRelaxed) {
-                       isRelaxed = relax();
+                       c.relax(currentPosition, target, dir); 
+                        currentPosition = c.getNewPosition();
+                        dir = c.getNewDirection();
+                        if (currentPosition.equals(target)) {
+                            isRelaxed = false;
+                        }
                    } else {
-                       findPath(currentPosition, targetPosition(pacMan), super.dir);
-                       move(targetPosition(pacMan));
+                      c.chase(currentPosition, pacMan, dir, Optional.empty());
+                      currentPosition = c.getNewPosition();
+                      dir = c.getNewDirection();
                    }
             }
             if (currentPosition.equals(pacMan.getPosition())) {
@@ -39,6 +37,5 @@ public class Clyde extends GhostAbstractImpl {
             } else {
                   return 0;
             }
-
         }
 }
