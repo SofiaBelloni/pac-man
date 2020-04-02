54ae206cd940219acd180a3b786b85f4e161d3d3
Davide Rossi <davide.rossi39@studio.unibo.it>
2020-04-02 21:12:53 +0200

    fixed some warnings

--- src/main/java/model/GhostFactoryImpl.java
+++ src/main/java/model/GhostFactoryImpl.java
@@ -4,23 +4,21 @@ import java.util.Set;
 
 
 public class GhostFactoryImpl implements GhostFactory {
-    private final Set<PairImpl<Integer,Integer>> setWall;
-    private final int xMap;
-    private final int yMap;
+    private final Set<PairImpl<Integer, Integer>> setWall;
+    private int xMap;
+    private int yMap;
 
-    public GhostFactoryImpl(Set<PairImpl<Integer,Integer>> setWall, Integer xMap, Integer yMap) {
+    public GhostFactoryImpl(final Set<PairImpl<Integer, Integer>> setWall) {
         this.setWall = setWall;
-        this.xMap = xMap;
-        this.yMap = yMap;
     }
-    
+
     @Override
     public Ghost blinky() { 
-        return new GhostAbstractImpl(xMap, yMap) {        
+        return new GhostAbstractImpl(this.xMap, this.yMap) {        
             public void create() {
-                this.relaxTarget = new PairImpl<>(xMap,yMap);
+                this.relaxTarget = new PairImpl<>(xMap, yMap);
                 this.myBehaviour = new BlinkyBehaviour(setWall, xMap, yMap, this.relaxTarget);
-                this.initialPosition = new PairImpl<>(7,6);
+                this.initialPosition = new PairImpl<>(7, 6);
                 this.myBehaviour.setPosition(this.initialPosition);
             }
         };
@@ -28,11 +26,11 @@ public class GhostFactoryImpl implements GhostFactory {
 
     @Override
     public Ghost pinky() {
-        return new GhostAbstractImpl(xMap, yMap) {        
+        return new GhostAbstractImpl(this.xMap, this.yMap) {        
             public void create() {
                 this.relaxTarget = new PairImpl<>(0,yMap);
                 this.myBehaviour = new PinkyBehaviour(setWall, xMap, yMap, this.relaxTarget);
-                this.initialPosition = new PairImpl<>(7,6);
+                this.initialPosition = new PairImpl<>(7, 6);
                 this.myBehaviour.setPosition(this.initialPosition);
             }
         };
@@ -40,9 +38,9 @@ public class GhostFactoryImpl implements GhostFactory {
 
     @Override
     public Ghost inky(Ghost blinky) {
-        return new GhostAbstractImpl(xMap, yMap) {        
+        return new GhostAbstractImpl(this.xMap, this.yMap) {        
             public void create() {
-                this.relaxTarget = new PairImpl<>(xMap,0);
+                this.relaxTarget = new PairImpl<>(xMap, 0);
                 this.myBehaviour = new InkyBehaviour(setWall, xMap, yMap, this.relaxTarget);
                 this.initialPosition = new PairImpl<>(7,6);
                 this.myBehaviour.setPosition(this.initialPosition);
@@ -52,11 +50,11 @@ public class GhostFactoryImpl implements GhostFactory {
 
     @Override
     public Ghost clyde() {
-        return new GhostAbstractImpl(xMap, yMap) {        
+        return new GhostAbstractImpl(this.xMap, this.yMap) {        
             public void create() {
-                this.relaxTarget = new PairImpl<>(0,0);
+                this.relaxTarget = new PairImpl<>(0, 0);
                 this.myBehaviour = new ClydeBehaviour(setWall, xMap, yMap, this.relaxTarget);
-                this.initialPosition = new PairImpl<>(7,6);
+                this.initialPosition = new PairImpl<>(7, 6);
                 this.myBehaviour.setPosition(this.initialPosition);
             }
         };
