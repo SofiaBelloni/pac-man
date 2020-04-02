dcbe701a2523f29e8ec7cafc280cbe557263f3f6
FILIPPO VISSANI <filippo.vissani@studio.unibo.it>
2020-04-02 16:18:02 +0200

    Added javadoc to Pair

--- src/main/java/model/Pair.java
+++ src/main/java/model/Pair.java
@@ -1,9 +1,19 @@
 package model;
 
+/**
+ *
+ * @param <X>
+ * @param <Y>
+ * 
+ * this interface manages a pair of objects
+ */
 public interface Pair<X, Y> {
-
+    /**
+     * @return the element of type X
+     */
     X getX();
-
+    /**
+     * @return the element of type Y
+     */
     Y getY();
-
 }
