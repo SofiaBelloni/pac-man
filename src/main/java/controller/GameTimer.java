1ecda0c609199a3cc962fd90eeb17bc463aa9128
Sofia Belloni <sofia.belloni@studio.unibo.it>
2020-04-05 21:59:30 +0200

    Modified GameTimer. Added LevelTimer class that is used to start and stop the GameTimer.

--- src/main/java/controller/GameTimer.java
+++ src/main/java/controller/GameTimer.java
@@ -1,20 +1,32 @@
 package controller;
 
 import java.util.TimerTask;
-
+import model.GameModel;
+/**
+ * This class represents the timer of each level.
+ */
 public class GameTimer extends TimerTask {
-    
-    private GameModel model;
 
-    public GameTimer(final GameModel model) {
+    private final GameModel model;
+    private boolean isRunning;
+    /**
+     * Constructor.
+     * @param model
+     *      the model reference
+     * @param isRunning
+     *      represents the run method state
+     */
+    public GameTimer(final GameModel model, final boolean isRunning) {
         this.model = model;
+        this.isRunning = isRunning;
     }
 
+    @Override
     public void run() {
-        if (this.model.getLevelTime() > 1) {
+        if (this.model.getLevelTime() > 0) {
             this.model.decLevelTime();
         } else {
-            this.model.nextLevel();
+            this.isRunning = false;
             this.cancel();
         }
     }
---
+++ src/main/java/controller/LevelTimer.java
@@ -0,0 +1,43 @@
+package controller;
+
+import java.util.Timer;
+
+import model.GameModel;
+/**
+ * This class is used to start and stop the GameTimer.
+ */
+public class LevelTimer {
+
+    private GameModel model;
+    private Timer levelTimer;
+    private GameTimer gameTimer;
+    private boolean isRunning;
+
+    /**
+     * Constructor.
+     * @param model
+     *      the model reference
+     */
+    public LevelTimer(final GameModel model) {
+        this.model = model;
+        this.levelTimer = new Timer();
+        this.isRunning = false;
+    }
+    /**
+     * Starts the GameTimer.
+     **/
+    public void startTimer() {
+        gameTimer = new GameTimer(this.model, this.isRunning);
+        this.levelTimer.scheduleAtFixedRate(gameTimer, 0, 1000);
+        this.isRunning = true;
+    }
+    /**
+     * Interrupts the GameTimer if it is running.
+     **/
+    public void stopTimer() {
+        if (this.isRunning) {
+            this.gameTimer.cancel();
+        }
+    }
+    
+}
