f676fa02ed1763005a7f5319061fe9d387247c0e
FILIPPO VISSANI <filippo.vissani@studio.unibo.it>
2020-04-18 12:19:15 +0200

    Added View interface

---
+++ src/main/java/view/View.java
@@ -0,0 +1,24 @@
+package view;
+
+import javafx.scene.Scene;
+
+public interface View {
+    /**
+     * @param controller
+     * set the controller of the view
+     */
+    void setController(Controller controller);
+    /*
+     * set the view to visible
+     */
+    void setVisible();
+    /*
+     * update the view
+     */
+    void render();
+    /**
+     * @param scene
+     * set the view Scene
+     */
+    void setScene(Scene scene);
+}
