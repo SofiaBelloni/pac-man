ab1fbeccd665675812bbe874b35f6181fa75ce62
FILIPPO VISSANI <filippo.vissani@studio.unibo.it>
2020-04-18 10:39:02 +0200

    Deleted observer package

--- src/main/java/observer/EventObserver.java
+++
@@ -1,9 +0,0 @@
-package observer;
-
-public interface EventObserver<T> {
-    /**
-     * @param source of the event
-     * @param event to send to observer
-     */
-    void update(ObservableEvent<T> source, T event);
-}
