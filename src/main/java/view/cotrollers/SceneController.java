cfebfc1487f20343628f987aaa8e8c8634531bf2
Eddie Barzi <eddie.barzi@studio.unibo.it>
2020-04-19 10:13:17 +0200

    Abstract class for the view controllers

---
+++ src/main/java/view/cotrollers/SceneController.java
@@ -0,0 +1,43 @@
+package view.cotrollers;
+
+import controller.Controller;
+import view.View;
+
+/**
+ * This represent the Abstract Class that define different Menu scenes.
+ */
+public abstract class SceneController {
+
+    private Controller controller;
+    private View view;
+
+    /**
+     * Method that initialized the controller and the view.
+     * @param controller
+     *      the controller of the game.
+     * @param view 
+     *      the controller of the view.
+     */
+    public SceneController(final Controller controller, final View view) {
+        this.controller = controller;
+        this.view = view;
+    }
+
+    /**
+     * Method getter of the Controller.
+     * @return
+     *      the controller.
+     */
+    protected Controller getController() {
+        return this.controller;
+    }
+
+    /**
+     * Method getter of the View.
+     * @return
+     *      the view.
+     */
+    protected View getView() {
+        return this.view;
+    }
+}
