1d6c71715e2cdba0837c5bc2e2d7d71c68381d98
Davide Rossi <davide.rossi39@studio.unibo.it>
2020-04-23 12:15:17 +0200

    Started implementation of MainMenuController

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -2,7 +2,6 @@ package view;
 
 import controller.Controller;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
 
 public interface View {
     void setController(Controller controller);
@@ -19,8 +18,4 @@ public interface View {
      * set the view Scene
      */
     void setScene(Scene scene);
-    /**
-     * get the stage.
-     */
-    void close();
 }
