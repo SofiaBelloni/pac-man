eb93531e6c088972ff53aa1dab850f0cea32e2de
Eddie Barzi <eddie.barzi@studio.unibo.it>
2020-04-25 12:02:03 +0200

    Update GameOverController.java from viewTesting

--- src/main/java/view/controllers/GameOverController.java
+++ src/main/java/view/controllers/GameOverController.java
@@ -1,38 +1,38 @@
 package view.controllers;
 
 import controller.Controller;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
+import javafx.scene.control.TextField;
 import view.View;
 
 public final class GameOverController extends SceneController {
 
     @FXML
-    private Label labelName;
+    private TextField textUserName;
 
     @FXML
-    private Label labelScore;
+    private Button buttonSave;
 
-    public GameOverController(final Controller controller, final View view) {
-        super(controller, view);
-        this.labelScore.setText("Name: "); // + controller.getUser().map(u -> u.getName()).orElse("Guest"));
-        this.labelName.setText("Score: "); // + controller.getCurrentTotalScore());
+    @FXML
+    private Label labelScoreHere;
+
+    public void init(final Controller controller, final View view) {
+        super.init(controller, view);
+        this.labelScoreHere.setText(String.valueOf(controller.getHighScore()));
     }
 
     @FXML
-    void onKeyPressed(final KeyEvent event) {
-        if (event.getCode() == KeyCode.ENTER) {
-            // QUI PASSARE ALLA SCHERMATA LEADERBOARD
-            Alert alert = new Alert(AlertType.INFORMATION);
-            alert.setTitle("Information Dialog");
-            alert.setHeaderText(null);
-            alert.setContentText("Hai premuto il tasto ENTER!");
+    void saveAndGoNextScene(final ActionEvent event) {
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Hai premuto il bottone SAVE");
+        alert.setHeaderText(null);
+        alert.setContentText("QUI PASSARE ALLA SCHERMATA LEADERBOARD!");
 
-            alert.showAndWait();
-        }
+        alert.showAndWait();
     }
 }
