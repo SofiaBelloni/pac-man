31d98a3850a3a8627e23804a33f7d8847c06a000
Eddie Barzi <eddie.barzi@studio.unibo.it>
2020-05-01 11:24:24 +0200

    Removed unused imports

--- src/main/java/view/controllers/GameOverController.java
+++ src/main/java/view/controllers/GameOverController.java
@@ -6,15 +6,10 @@ import java.util.Optional;
 import controller.Controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
-import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import view.GameScene;
-import view.SceneLoader;
 import view.View;
 
 public final class GameOverController extends SceneController {
