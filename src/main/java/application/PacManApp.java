95848d114618604bcb6b4eeb8a7e79c9f50e2323
Eddie Barzi <eddie.barzi@studio.unibo.it>
2020-05-09 00:30:39 +0200

    Removed unused imports and PMD warnings

--- src/main/java/application/PacManApp.java
+++ src/main/java/application/PacManApp.java
@@ -2,27 +2,12 @@ package application;
 
 import controller.Controller;
 import controller.ControllerImpl;
-import controller.GameMapLoader;
-import controller.GameMapLoaderImpl;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyCombination;
 import javafx.stage.Stage;
-import model.GameMap;
-import model.GameMapImpl;
 import model.GameModel;
 import model.GameModelImpl;
-import utils.Pair;
-import view.GameScene;
-import view.SceneLoader;
 import view.View;
 import view.ViewImpl;
-import view.controllers.GameViewController;
-import view.controllers.MainMenuController;
-import view.controllers.SceneController;
 
 /**
  * This class represent the Main class of the JavaFX-based application.
@@ -32,8 +17,8 @@ public final class PacManApp extends Application {
     @Override
     public void start(final Stage stage) throws Exception {
         final GameModel model = new GameModelImpl();
-        View view = new ViewImpl(stage);
-        Controller controller = new ControllerImpl(model, view);
+        final View view = new ViewImpl(stage);
+        final Controller controller = new ControllerImpl(model, view);
         view.setController(controller);
     }
 
